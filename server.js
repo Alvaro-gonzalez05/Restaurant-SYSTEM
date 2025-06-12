cadf01631532ca4719d1a67e50cc15948318b709
xAlva69 <as.gonzalez@alumno.etec.um.edu.ar>
2025-06-12 13:03:59 -0300

    avance

---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const http = require('http');
+const { Server } = require('socket.io');
+
+const PORT = process.env.WEBSOCKET_PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('WebSocket server running');
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('Cliente conectado al WebSocket');
+
+  socket.on('message', (message) => {
+    console.log('Mensaje recibido:', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado del WebSocket');
+  });
+});
+
+// Función para emitir un nuevo pedido desde otro archivo si lo necesitas
+module.exports.notifyNewOrder = (order) => {
+  io.emit('new-order', order);
+};
+
+server.listen(PORT, () => {
+  console.log(`Servidor WebSocket escuchando en el puerto ${PORT}`);
+});
